d85c4c5cfc9d2f9804c067cd78cbbc93653a9967
pablinchapin <pablinchapin@gmail.com>
2021-10-11 21:38:35 -0600

    Adding Transaction ID and Contract ID data to fix missing data

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,10 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: process.env.HOST,
+      port: process.env.PORT,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
